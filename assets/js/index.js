1f5384667c53cf3a5fae2a5487694ff9994e1e1a
Daniel Tello Gaete <dtellogaete@gmail.com>
2022-11-27 16:27:43 -0600

    button js

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+const setData = () =>{
+    /* Input Data*/ 
+    const quantity = document.querySelector('#inputQuantity').value;
+    const totalQuantity = quantity*400000;
+    const color = document.querySelector('#inputColor').value;
+
+    if(quantity != '' && color != ''){ 
+        /*Set Data*/      
+        document.querySelector('#total').innerHTML = totalQuantity;
+        document.querySelector('#quantity').innerHTML = quantity;        
+        document.querySelector('#color').style.backgroundColor = color;
+    };    
+};
+
+document.querySelector('#btn-calculate').addEventListener('click', setData);
+
+
+
+
+
+
